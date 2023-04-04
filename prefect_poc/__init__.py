cb1dec95fd1adad48cbb853ddbb359130148b772
jan.malek <jan.malek@ki-insurance.com>
2023-04-04 13:03:47 +0100

    feature: Workflows from a separate branch of the same repo

---
+++ prefect_poc/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT_DIR = os.path.dirname(THIS_DIR)
+WORKFLOW_DIR = os.path.join(PROJECT_ROOT_DIR, "workflows")
--- prefect_poc/blocks/poc_github.py
+++ prefect_poc/blocks/poc_github.py
@@ -1,12 +1,15 @@
 from prefect.filesystems import GitHub
+from prefect_poc import PROJECT_ROOT_DIR
 
 
 def build_block():
     block = GitHub(
-        repository="https://github.com/jmalek-ki/dp-prefect-poc.git",
+        repository="https://github.com/jmalek-ki/dp-prefect-poc",
+        reference="workflows"
     )
-    block.get_directory("workflows") # specify a subfolder of repo
-    block.save("poc-repo")
+
+    block.get_directory("workflows", PROJECT_ROOT_DIR)  # specify a subfolder of repo
+    block.save("poc-repo", overwrite=True)
 
 
 if __name__ == '__main__':
