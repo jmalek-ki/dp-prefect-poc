a344f9a6a79302a30b1c2790ef41f155966314ee
jan.malek <jan.malek@ki-insurance.com>
2023-04-04 13:57:21 +0100

    fix: Autodiscovery/autodeployment working properly.

--- prefect_poc/deployments/poc_github.py
+++ prefect_poc/deployments/poc_github.py
@@ -8,8 +8,8 @@ from prefect_poc import PROJECT_ROOT_DIR, WORKFLOW_DIR, WORKFLOW_DIR_SUFFIX
 from prefect_poc.blocks import poc_github
 
 
-async def deploy_flow(flow):
-    deployment = await Deployment.build_from_flow(flow)
+async def deploy_flow(flow, name):
+    deployment = await Deployment.build_from_flow(flow, name)
     deployment_id = await deployment.apply()
     return deployment_id
 
@@ -17,11 +17,17 @@ async def deploy_flow(flow):
 async def deploy_all_flows():
     await poc_github.build_block(workflow_dir=PROJECT_ROOT_DIR)
     flow_files = glob.iglob("*.py", root_dir=WORKFLOW_DIR)
-    built_deployments = set()
+    built_deployments = dict()
 
     for flow_file in flow_files:
+        if flow_file.startswith("__"):
+            continue
+
+        module_name = flow_file[:-3]
+        import_name = f"{WORKFLOW_DIR_SUFFIX}.{module_name}"
+
         try:
-            flow_mod = importlib.import_module(flow_file, WORKFLOW_DIR_SUFFIX)
+            flow_mod = importlib.import_module(import_name)
         except ImportError as IErr:
             print(IErr)
             continue
@@ -30,9 +36,11 @@ async def deploy_all_flows():
         if not entrypoint:
             continue
 
-        deployment_id = await deploy_flow(entrypoint)
-        built_deployments.add(deployment_id)
+        deployment_name = getattr(flow_mod, "deployment_name", module_name)
+        deployment_id = await deploy_flow(entrypoint, deployment_name)
+        built_deployments[deployment_name] = deployment_id
 
+    print(f"BUILT DEPLOYMENTS: {built_deployments}")
     return built_deployments
 
 
