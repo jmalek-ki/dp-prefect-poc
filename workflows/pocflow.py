b0b4f11681ceb84ef53c36793ac240c13db39b05
jan.malek <jan.malek@ki-insurance.com>
2023-04-10 13:46:21 +0100

    feature: Added artifact generation to test out that feature.

--- workflows/pocflow.py
+++ workflows/pocflow.py
@@ -1,5 +1,7 @@
+import datetime
 import requests
 from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
 
 
 @task
@@ -16,10 +18,22 @@ def parse_fact(response):
     return fact
 
 
+@task
+def create_artifact(fact):
+    now_time = str(datetime.datetime.utcnow())
+    artifact_id = create_markdown_artifact(
+        key=now_time,
+        description=f"Your cat fact for {now_time}!",
+        markdown=f"# Fact:\n\n{fact}"
+    )
+    return artifact_id
+
+
 @flow
 def api_workflow(url):
     fact_json = call_api(url)
     fact_text = parse_fact(fact_json)
+    create_artifact(fact_text)
     return fact_text
 
 
