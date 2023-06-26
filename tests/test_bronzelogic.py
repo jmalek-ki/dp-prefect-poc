a888e36dec4839dfb10157dd10bf8f81004828aa
jan.malek <jan.malek@ki-insurance.com>
2023-06-26 12:28:16 +0100

    fix: Fixed a typo (utilitites => utilities)

--- tests/test_bronzelogic.py
+++ tests/test_bronzelogic.py
@@ -8,7 +8,7 @@ import pytest
 from gcp_storage_emulator.server import create_server
 from google.cloud import storage
 
-from utilitites import bronzelogic
+from utilities import bronzelogic
 
 MOCK_TIME_ISO = "2020-01-01T22:11"
 MOCK_TIME_DATETIME = datetime.datetime.fromisoformat(MOCK_TIME_ISO)
